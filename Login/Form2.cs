e9e44a20b81d67645fd168950fdb15c782869231
ronlocode <ron910517@gmail.com>
2022-10-25 20:46:57 +0800

    Add spd, move and timer1_Tick

--- Login/Form2.cs
+++ Login/Form2.cs
@@ -8,41 +8,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Login
-{
-    public partial class Form2 : Form
-    {
-        static Graphics g;	  //繪圖裝置（一個就夠了）
+namespace Login {
+    public partial class Form2 : Form {
+        static double fr = 0; // 摩擦力
+        static Graphics g; //繪圖裝置（一個就夠了）
         static int r = 10, r2 = 20;    //半徑，直徑
 
-        class ball
-        {     //球 class(類別)
+        class ball { //球 class(類別)
+            public double spd = 0; // 球 行進速度
+            public void move() { // 移動球
+                if (spd > 0) { // 速度>0才移動
+                    x += spd * cosA; // x 分量
+                    y += spd * sinA; // y 分量
+                    spd -= fr; // 速度依摩擦力遞減
+                } else spd = 0; // 避免spd <０造成反向移動
+            }
             int id;                                      //球編號
             public double x = 0, y = 0;    //球心 坐標
             Color c;                                   //球顏色
             SolidBrush br;                        //刷子（畫球用）
             private double ang = 0;       //ex4：球 行進角度
             public double cosA, sinA;   //ex4：coSine 行進角度, Sine 行進角度
-            public ball(int bx, int by, Color cc, int i)
-            {  //建構者
+            public ball(int bx, int by, Color cc, int i) {  //建構者
                 x = bx;                                 //球心 x 坐標
                 y = by;                                 //球心 y 坐標
                 c = cc;                                  //球顏色    
                 br = new SolidBrush(cc);     //球顏色的刷子
                 id = i;                                   //球編號
             }
-            public void draw()
-            {      //畫 球物件 自己
+            public void draw() {      //畫 球物件 自己
                 g.FillEllipse(br, (int)(x - r), (int)(y - r), r2, r2);          //畫橢圓（球刷子，左上角 坐標，直徑寬，直徑高）
             }
-            public void setAng(double _ang)
-            {    //ex4：角度 改變
+            public void setAng(double _ang) {    //ex4：角度 改變
                 ang = _ang;                       // 存 新角度
                 cosA = Math.Cos(ang);     //  重算 coSine
                 sinA = Math.Sin(ang);       //  重算 Sine
             }
-            public void drawStick()
-            {    //ex4：畫球桿
+            public void drawStick() {    //ex4：畫球桿
                 double r12 = 12 * r;
                 Pen skyBluePen = new Pen(Brushes.DeepSkyBlue);    // 宣告 + new 深藍色  畫筆
                 skyBluePen.Width = 3.0F;   // 改變 畫筆 寬度
@@ -52,10 +54,16 @@ namespace Login
                 );                                                        // - r12   -r , 使球杆 畫在滑鼠點的另一邊
             }
         }	//class ball 結束
-
+        private void timer1_Tick(object sender, EventArgs e) {
+            double sum_spd = 0; // 球的速度加總
+            panel1.Refresh(); // 呼叫panel1_Paint事件處理副程式
+            for(int i = 0 ; i < 10 ; i++) {
+                balls[i].move(); // 移動球
+                sum_spd += balls[i].spd;
+            }
+        }
         ball[] balls = new ball[10];    // 10 顆球的陣列    宣告，new 
-        public Form2()
-        {
+        public Form2() {
             InitializeComponent();
             g = panel1.CreateGraphics();     //繪圖裝置 初始化
             for (int i = 1; i < 10; i++)            //new 每個球，ball 建構者參數 見 work_note3 說明
@@ -67,35 +75,30 @@ namespace Login
         }
 
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
+        private void panel1_Paint(object sender, PaintEventArgs e) {
             for (int i = 0; i < 10; i++)     //10 顆球
                 balls[i].draw();     //每個球 畫自己
 
             balls[0].drawStick();     //  ex4：畫指向 0號球(母球) 的球桿
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void button1_Click(object sender, EventArgs e) {
             this.Hide();
             Owner.Show();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+        private void label1_Click(object sender, EventArgs e){
             
         }
 
-        private void panel1_MouseDown_1(object sender, MouseEventArgs e)
-        {
+        private void panel1_MouseDown_1(object sender, MouseEventArgs e) {
             double a = Math.Atan2(e.Y - balls[0].y, e.X - balls[0].x); // e:滑鼠 點擊處坐標
             balls[0].setAng(a); // 存入母球 行進角度
             panel1.Refresh(); // 重新繪畫轉動過的球桿
             g.DrawRectangle(Pens.HotPink, e.X - 2, e.Y - 2, 4, 4); // 點擊點 畫小方塊
         }
 
-        private void Form2_Load(object sender, EventArgs e)
-        {
+        private void Form2_Load(object sender, EventArgs e) {
             label1.Text = ((Form1)Owner).textBox2.Text + "你好!";
         }
     }
